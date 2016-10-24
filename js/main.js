a29b420ac63c9f4a99da6ccb038132352cb2d9d4
Tai Luis <tailuis7@gmail.com>
2016-10-24 13:02:22 +0700

    Done Header and Footer. Doing Project Page.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
 $(document).ready(function() {
-    
+
+    var maxHeight = $('.navbar-default').height();
+    $('#banner_backing').height(maxHeight);
+
+    /*var maxProjectWidth = -1;
+    $('.project img').each(function() {
+        maxProjectWidth = maxProjectWidth > $(this).width() ? maxProjectWidth : $(this).width();
+    });
+    $('.project_image_overlay').width(maxProjectWidth);*/
+
+
+    $(document).scroll(function() {
+        if ($(this).scrollTop() > 0) {
+            $('#banner_backing').css({ "opacity": "0" });
+        }
+        if ($(this).scrollTop() > 70) {
+            $('#banner_backing').css({ "opacity": "0" });
+        }
+        if ($(this).scrollTop() > 80) {
+            $('#banner_backing').css({ "opacity": "0.3" });
+        }
+        if ($(this).scrollTop() > 90) {
+            $('#banner_backing').css({ "opacity": "0.6" });
+        }
+        if ($(this).scrollTop() > 100) {
+            $('#banner_backing').css({ "opacity": "0.9" });
+        }
+    });
 });
